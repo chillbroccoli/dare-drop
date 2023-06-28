d08439b7ca58cc1414b5b9c03900af338831f215
Kacper Malek <kacper.malek0828@gmail.com>
2023-06-28 16:24:37 +0200

    code refactor

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,11 +1,12 @@
 import { Server, Socket } from "socket.io";
 
+import { EVENTS } from "./constants/events";
 import { logger } from "./utils/logger";
 
 export function socket({ io }: { io: Server }) {
   logger.info("Sockets enabled");
 
-  io.on("connection", (socket: Socket) => {
+  io.on(EVENTS.CONNECTION, (socket: Socket) => {
     logger.info(`User connected: ${socket.id}`);
   });
 }
